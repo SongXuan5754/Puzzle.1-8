352cc4843e50329d0e711c3d117fda3f6f0cf858
SongXuan5754 <xsong@cca.edu>
2016-01-30 00:04:31 -0800

    Create puzzle4.js

---
+++ puzzle4.js
@@ -0,0 +1,20 @@
+down();
+down();
+down();
+down();
+right();
+var c1 = getColor();
+
+down();
+setColor(c1);
+right();
+
+right();
+up();
+right();
+var c2 = getColor();
+
+down();
+setColor(c2);
+right();
+right();
