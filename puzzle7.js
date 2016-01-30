b7fb61801d687e692200f35b207932b8737abe4c
SongXuan5754 <xsong@cca.edu>
2016-01-30 00:08:56 -0800

    Create puzzle7.js

---
+++ puzzle7.js
@@ -0,0 +1,33 @@
+while (getColor() != "red") {
+  down();
+}
+
+right();
+right();
+
+while (getColor() != "red") {
+  up();
+}
+
+right();
+right();
+
+while (getColor() != "red") {
+  down();
+}
+
+right();
+right();
+
+while (getColor() != "red") {
+  up();
+}
+
+right();
+right();
+
+while (getColor() != "red") {
+  down();
+}
+
+right();
