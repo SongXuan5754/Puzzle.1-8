1c5ebd0577619f81248756043d385bdb73b46d7b
SongXuan5754 <xsong@cca.edu>
2016-01-30 00:08:22 -0800

    Create puzzle8.js

---
+++ puzzle8.js
@@ -0,0 +1,44 @@
+var m = 0;
+
+while (m < 8){
+  down();
+  m = m + 1;
+}
+
+right();
+right();
+m = 0;
+
+while (m < 8){
+  up();
+  m = m + 1;
+}
+
+right();
+right();
+m = 0;
+
+while (m < 8){
+  down();
+  m = m + 1;
+}
+
+right();
+right();
+m = 0;
+
+while (m < 8){
+  up();
+  m = m + 1;
+}
+
+right();
+right();
+m = 0;
+
+while (m < 8){
+  down();
+  m = m + 1;
+}
+
+right();
